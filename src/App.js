4368c3731168b1a86f9d4b2de6dfb9d2ac4009e1
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2022-12-06 06:37:02 +0100

    Add post route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import Main from "./components/Main";
 import About from "./components/About";
 import Terms from "./components/Terms";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import { useState } from "react";
+import CreatePost from "./components/CreatePost";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:3000";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(
+    Boolean(localStorage.getItem("reachMeAppToken"))
+  );
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={loggedIn ? <Home /> : <Main />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/create-post" element={<CreatePost />} />
         <Route path="/about-us" element={<About />} />
       </Routes>
       <Footer />
