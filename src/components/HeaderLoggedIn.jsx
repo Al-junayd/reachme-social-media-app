5b838d4c10b00e14357492994175810377dc2e81
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2022-12-06 06:37:47 +0100

    Allow users to login

--- src/components/HeaderLoggedIn.jsx
+++ src/components/HeaderLoggedIn.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeaderLoggedIn = (props) => {
+  function handleLogout() {
+    props.setLoggedIn(false);
+    localStorage.removeItem("reachMeAppToken");
+    localStorage.removeItem("reachMeAppUsername");
+    localStorage.removeItem("reachMeAppAvatar");
+  }
   return (
     <div className="flex-row my-3 my-md-0">
       <a href="#" className="text-white mr-2 header-search-icon">
@@ -14,15 +21,15 @@ const HeaderLoggedIn = (props) => {
         <img
           alt="avatar"
           className="small-header-avatar"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
+          src={localStorage.getItem("reachMeAppAvatar")}
         />
       </a>
-      <a className="btn btn-sm btn-success mr-2" href="/create-post">
+      <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </a>
+      </Link>
       <button
         type="submit"
-        onClick={() => props.setLoggedIn(false)}
+        onClick={handleLogout}
         className="btn btn-sm btn-secondary"
       >
         Sign Out
